c2a03004b2bc0c9e253a360ae6d50376134ebe56
sarahnj <iga.hanyn.sara@gmail.com>
2018-04-15 11:55:58 +0000

    groups access to all models

--- parc_auto/models/ordremission.py
+++ parc_auto/models/ordremission.py
@@ -7,20 +7,18 @@ class ordremission(models.Model):
     name = fields.Char(string='Ordre')
     ordre_id = fields.Char('Ordre Mission N°')
 
-    current_user = fields.Many2one('res.users', compute='_get_current_user')
-
     state = fields.Selection([
         ('draft', "Draft"),
         ('submitted', "Submitted"),
         ('confirmed', "Confirmed"),
         ('done', "Done"),
     ])
-    chauffeur_id=fields.Many2one('parcauto.chauffeur', 'Chauffeur')
-    agence_dep=fields.Many2one('parcauto.agence', 'Agence Départ')
-    agence_arr = fields.Many2one('parcauto.agence', 'Agence Arrivée')
-    client_id=fields.Many2one('parcauto.client', 'Clients')
-    produit_id=fields.Many2one('parcauto.produit', 'Colis')
-    vehicule_id=fields.Many2one('parcauto.vehicule',ondelete='set null', string="Véhicule", index=True)
+    chauffeur_id=fields.Many2one('parcauto.chauffeur', 'Chauffeur',required=True)
+    agence_dep=fields.Many2one('parcauto.agence', 'Agence Départ',required=True)
+    agence_arr = fields.Many2one('parcauto.agence', 'Agence Arrivée',required=True)
+    client_id=fields.Many2one('parcauto.client', 'Clients',required=True)
+    produit_id=fields.Many2one('parcauto.produit', 'Colis',required=True)
+    vehicule_id=fields.Many2one('parcauto.vehicule',ondelete='set null', string="Véhicule",required=True, index=True)
 
 
     @api.model
@@ -45,13 +43,6 @@ class ordremission(models.Model):
     @api.multi
     def action_done(self):
         self.state = 'done'
-
-    @api.depends()
-    def _get_current_user(self):
-        for rec in self:
-            rec.current_user = self.env.user
-        # i think this work too so you don't have to loop
-        self.update({'current_user' : self.env.user.id})
 
 class chauffeur(models.Model):
     _name='parcauto.chauffeur'
